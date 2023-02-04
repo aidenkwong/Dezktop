5840a764015a3cd80f324054a9273d1dc7b4661c
aidenkwong <aidenkwong5@gmail.com>
2023-02-04 10:06:56 -0500

    change document title to Dezktop

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home() {
   return (
     <div className={theme}>
       <Head>
-        <title>Desktop</title>
+        <title>Dezktop</title>
         <meta name="description" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
